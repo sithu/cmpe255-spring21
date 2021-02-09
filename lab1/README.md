a6287162c628f0c1d7d447f04bdf738660565fb2
sithu <a.sithu@gmail.com>
2021-02-09 11:27:53 -0800

    add lab1

---
+++ lab1/README.md
@@ -0,0 +1,26 @@
+# Lab 1
+
+Basic data manipulation using Pandas and Numpy libraries.
+
+Check out [Chipotle Exercises Video Tutorial](https://www.youtube.com/watch?v=lpuYZ5EUyS8&list=PLgJhDSE2ZLxaY_DigHeiIDC1cD09rXgJv&index=2) to watch a data scientist go through the exercises
+
+We will be using [this dataset](https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv) in the lab 1.
+
+
+## Install Dependency
+
+We will be using [pipenv](https://realpython.com/pipenv-guide/) to manage dependencies. Run this command to install required dependencies.
+
+```
+pipenv install -r requirements.txt
+```
+
+
+Let's implement all ##TODO functions in lab1.py.
+
+## To Run the lab 1
+
+```
+pipenv shell
+python lab1.py | tee lab1_output.txt
+```
---
+++ lab1/lab1.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+
+class Solution:
+    def __init__(self) -> None:
+        # TODO: 
+        # Load data from data/chipotle.tsv file using Pandas library and 
+        # assign the dataset to the 'chipo' variable.
+        file = 'data/chipotle.tsv'
+        self.chipo = 'FIXME'
+    
+    def top_x(self, count) -> None:
+        # TODO
+        # Top x number of entries from the dataset and display as markdown format.
+        topx = 'FIXME'
+        print(topx.to_markdown())
+        
+    def count(self) -> int:
+        # TODO
+        # The number of observations/entries in the dataset.
+        return -1
+    
+    def info(self) -> None:
+        # TODO
+        # print data info.
+        pass
+    
+    def num_column(self) -> int:
+        # TODO return the number of columns in the dataset
+        return -1
+    
+    def print_columns(self) -> None:
+        # TODO Print the name of all the columns.
+        pass
+    
+    def most_ordered_item(self):
+        # TODO
+        item_name = None
+        order_id = -1
+        quantity = -1
+        return item_name, order_id, quantity
+
+    def total_item_orders(self) -> int:
+       # TODO How many items were orderd in total?
+       return -1
+   
+    def total_sales(self) -> float:
+        # TODO 
+        # 1. Create a lambda function to change all item prices to float.
+        # 2. Calculate total sales.
+        return 0.0
+   
+    def num_orders(self) -> int:
+        # TODO
+        # How many orders were made in the dataset?
+        return -1
+    
+    def average_sales_amount_per_order(self) -> float:
+        # TODO
+        return 0.0
+
+    def num_different_items_sold(self) -> int:
+        # TODO
+        # How many different items are sold?
+        return -1
+    
+
+def test() -> None:
+    solution = Solution()
+    solution.top_x(10)
+    count = solution.count()
+    print(count)
+    assert count == 4622
+    solution.info()
+    count = solution.num_column()
+    assert count == 5
+    item_name, order_id, quantity = solution.most_ordered_item()
+    assert item_name == 'Chicken Bowl'
+    assert order_id == 713926	
+    assert quantity == 159
+    total = solution.total_item_orders()
+    assert total == 4972
+    assert 39237.02 == solution.total_sales()
+    assert 1834 == solution.num_orders()
+    assert 21.39 == solution.average_sales_amount_per_order()
+    assert 50 == solution.num_different_items_sold()
+
+    
+if __name__ == "__main__":
+    # execute only if run as a script
+    test()
+    
+    
