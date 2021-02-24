160c6eeb08d1f10af5188ea7bfc1c0e495b671b2
Sithu <sithu@sithus-mbp.lan>
2021-02-23 18:01:13 -0800

    regression lecture

---
+++ lecture/regression/home-price.py
@@ -0,0 +1,97 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import utils
+
+features = np.array([1,2,3,5,6,7])
+labels = np.array([155, 197, 244, 356,407,448])
+
+print(features)
+print(labels)
+
+utils.plot_points(features, labels)
+
+def simple_trick(base_price, price_per_room, num_rooms, price):
+    # select random learning rate
+    small_random_1 = random.random()*0.1
+    small_random_2 = random.random()*0.1
+    # calculate the prediction.
+    predicted_price = base_price + price_per_room*num_rooms
+    # check where the point is with respect to the line.
+    if price > predicted_price and num_rooms > 0:
+        # translate the line
+        price_per_room += small_random_1
+        # rotate the line
+        base_price += small_random_2
+    if price > predicted_price and num_rooms < 0:
+        price_per_room -= small_random_1
+        base_price += small_random_2
+    if price < predicted_price and num_rooms > 0:
+        price_per_room -= small_random_1
+        base_price -= small_random_2
+    if price < predicted_price and num_rooms < 0:
+        price_per_room -= small_random_1
+        base_price += small_random_2
+    return price_per_room, base_price
+
+
+def absolute_trick(base_price, price_per_room, num_rooms, price, learning_rate):
+    predicted_price = base_price + price_per_room*num_rooms
+    if price > predicted_price:
+        price_per_room += learning_rate*num_rooms
+        base_price += learning_rate
+    else:
+        price_per_room -= learning_rate*num_rooms
+        base_price -= learning_rate
+    return price_per_room, base_price
+
+
+def square_trick(base_price, price_per_room, num_rooms, price, learning_rate):
+    predicted_price = base_price + price_per_room*num_rooms
+    price_per_room += learning_rate*num_rooms*(price-predicted_price)
+    base_price += learning_rate*(price-predicted_price)
+    return price_per_room, base_price
+
+
+# We set the random seed in order to always get the same results.
+random.seed(0)
+
+def linear_regression(features, labels, learning_rate=0.01, epochs = 1000):
+    price_per_room = random.random()
+    base_price = random.random()
+    for epoch in range(epochs):
+        # Uncomment any of the following lines to plot different epochs
+        if epoch == 1:
+            utils.draw_line(price_per_room, base_price, starting=0, ending=8)
+        elif epoch <= 10:
+            utils.draw_line(price_per_room, base_price, starting=0, ending=8)
+        elif epoch <= 50:
+            utils.draw_line(price_per_room, base_price, starting=0, ending=8)
+        elif epoch > 50:
+            utils.draw_line(price_per_room, base_price, starting=0, ending=8)
+        i = random.randint(0, len(features)-1)
+        num_rooms = features[i]
+        price = labels[i]
+        # Uncomment any of the 2 following lines to use a different trick
+        #price_per_room, base_price = absolute_trick(base_price,
+        price_per_room, base_price = square_trick(base_price,
+                                                  price_per_room,
+                                                  num_rooms,
+                                                  price,
+                                                  learning_rate=learning_rate)
+    utils.draw_line(price_per_room, base_price, 'black', starting=0, ending=8)
+    utils.plot_points(features, labels)
+    print('Price per room:', price_per_room)
+    print('Base price:', base_price)
+    return price_per_room, base_price
+
+# This line is for the x-axis to appear in the figure
+plt.ylim(0,500)
+
+linear_regression(features, labels, learning_rate = 0.01, epochs = 1000)
+
+# The root mean square error function
+def rmse(labels, predictions):
+    n = len(labels)
+    differences = np.subtract(labels, predictions)
+    return np.sqrt(1.0/n * (np.dot(differences, differences)))
