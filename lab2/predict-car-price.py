cce20a004baec433cff6d826da31c301c5011ed8
PRKKILLER <36620963+PRKKILLER@users.noreply.github.com>
2021-03-12 18:48:13 -0800

    add lab2

--- lab2/predict-car-price.py
+++ lab2/predict-car-price.py
@@ -9,6 +9,7 @@ class CarPrice:
     def __init__(self):
         self.df = pd.read_csv('data/data.csv')
         print(f'${len(self.df)} lines loaded')
+        
 
     def trim(self):
         self.df.columns = self.df.columns.str.lower().str.replace(' ', '_')
@@ -17,7 +18,37 @@ class CarPrice:
             self.df[col] = self.df[col].str.lower().str.replace(' ', '_')
 
     def validate(self):
-        pass
+        n = len(self.df)
+        n_val = int(0.2 * n)
+        n_test = int(0.2 * n)
+        n_train = n - (n_val + n_test)
+        idx = np.arange(n)
+        np.random.shuffle(idx)
+        df_shuffled = self.df.iloc[idx]
+
+        df_train = df_shuffled.iloc[:n_train].copy()
+        df_val = df_shuffled.iloc[n_train:n_train+n_val].copy()
+        df_test = df_shuffled.iloc[n_train+n_val:].copy()
+        y_train_orig = df_train.msrp.values
+        y_val_orig = df_val.msrp.values
+        y_test_orig = df_test.msrp.values
+       
+
+        y_train = np.log1p(df_train.msrp.values)
+        y_val = np.log1p(df_val.msrp.values)
+        y_test = np.log1p(df_test.msrp.values)
+
+        del df_train['msrp']
+        del df_val['msrp']
+        del df_test['msrp']
+        
+        return ((df_train,df_val['msrp'],df_test['msrp']),( y_train,y_val,y_test))
 
     def linear_regression(self, X, y):
-        pass
+        ones = np.ones(X.shape[0])
+        X = np.column_stack([ones, X])
+
+        XTX = X.T.dot(X)
+        XTX_inv = np.linalg.inv(XTX)
+        w = XTX_inv.dot(X.T).dot(y)
+        return w[0], w[1:]
