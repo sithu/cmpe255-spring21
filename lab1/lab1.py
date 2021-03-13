848f3020c1945235134f54182ec2bd337eb8c7ec
PRKKILLER <36620963+PRKKILLER@users.noreply.github.com>
2021-03-12 23:13:47 -0800

    feat lab2 added pic

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -8,68 +8,92 @@ class Solution:
         # Load data from data/chipotle.tsv file using Pandas library and 
         # assign the dataset to the 'chipo' variable.
         file = 'data/chipotle.tsv'
-        self.chipo = 'FIXME'
+        self.chipo = pd.read_csv(file, sep = '\t' )
+        print("shape:",self.chipo.shape)
     
     def top_x(self, count) -> None:
         # TODO
-        # Top x number of entries from the dataset and display as markdown format.
-        topx = 'FIXME'
-        print(topx.to_markdown())
+        # Top x number of entries from the dataset and display as markdown format.#####
+        self.topx = self.chipo.head(count)
+        print(self.topx.to_markdown())
         
     def count(self) -> int:
         # TODO
         # The number of observations/entries in the dataset.
-        return -1
+        return self.chipo.shape[0]
     
     def info(self) -> None:
         # TODO
         # print data info.
+        print(self.chipo.info())
         pass
     
     def num_column(self) -> int:
-        # TODO return the number of columns in the dataset
-        return -1
+        # TODO return the number of columns in the dataset.
+        return  len(self.chipo.columns)
     
+
     def print_columns(self) -> None:
         # TODO Print the name of all the columns.
+        print(self.chipo.columns)
         pass
     
     def most_ordered_item(self):
         # TODO
-        item_name = None
-        order_id = -1
-        quantity = -1
-        return item_name, order_id, quantity
+       
+        most_ordered =  self.chipo.groupby(["item_name"]).agg({"quantity":'sum'}).sort_values('quantity',ascending=False)
+        most_ordered = most_ordered.reset_index()
+        quantity=most_ordered["quantity"][0]
+        item_name=most_ordered["item_name"][0]
+        return item_name,quantity
 
     def total_item_orders(self) -> int:
        # TODO How many items were orderd in total?
-       return -1
+        return  self.chipo["quantity"].sum()
    
     def total_sales(self) -> float:
         # TODO 
         # 1. Create a lambda function to change all item prices to float.
         # 2. Calculate total sales.
-        return 0.0
+        ls=[]
+        self.chipo["item_price"] = pd.to_numeric(self.chipo["item_price"].str.slice(1))
+        ls=sum((self.chipo["item_price"]*self.chipo["quantity"]))
+        ls="{0:.2f}".format(ls)
+        
+        return float(ls)
    
     def num_orders(self) -> int:
         # TODO
         # How many orders were made in the dataset?
-        return -1
+        len(self.chipo["order_id"].unique())
+        return len(self.chipo["order_id"].unique())
     
     def average_sales_amount_per_order(self) -> float:
         # TODO
-        return 0.0
+        avg=sum((self.chipo["item_price"]*self.chipo["quantity"]))/ len(self.chipo["order_id"].unique())
+        avg="{0:.2f}".format(avg)
+    
+        return float(avg)
 
     def num_different_items_sold(self) -> int:
         # TODO
         # How many different items are sold?
-        return -1
+       
+        return len(self.chipo["item_name"].unique())
     
     def plot_histogram_top_x_popular_items(self, x:int) -> None:
         from collections import Counter
         letter_counter = Counter(self.chipo.item_name)
         # TODO
         # 1. convert the dictionary to a DataFrame
+        #print(letter_counter)
+        lc=pd.DataFrame(list(letter_counter.items()),columns=['ITEM','ITEM_PRICE']).sort_values("ITEM_PRICE",ascending=False)
+        lc.reset_index(drop=True, inplace=True)
+        lc=lc[:5]
+        plt.bar(lc["ITEM"],lc['ITEM_PRICE'])
+        plt.title("Most popular items")
+        plt.xlabel("Items")
+        plt.ylabel("Number of Orders")
         # 2. sort the values from the top to the least value and slice the first 5 items
         # 3. create a 'bar' plot from the DataFrame
         # 4. set the title and labels:
@@ -77,11 +101,22 @@ class Solution:
         #     y: Number of Orders
         #     title: Most popular items
         # 5. show the plot. Hint: plt.show(block=True).
+        plt.show(block=True)
         pass
         
     def scatter_plot_num_items_per_order_price(self) -> None:
         # TODO
         # 1. create a list of prices by removing dollar sign and trailing space.
+       
+     
+     
+        price=self.chipo.groupby("order_id").sum(["item_price","quantity"]).reset_index()
+      
+        plt.scatter(x=price["item_price"],y=price["quantity"],s=50,c='blue')
+        plt.title('Number of items per order price')
+        plt.xlabel('Order Price')
+        plt.ylabel('Num Item')
+        plt.show()
         # 2. groupby the orders and sum it.
         # 3. create a scatter plot:
         #       x: orders' item price
@@ -105,10 +140,9 @@ def test() -> None:
     solution.info()
     count = solution.num_column()
     assert count == 5
-    item_name, order_id, quantity = solution.most_ordered_item()
+    item_name,quantity = solution.most_ordered_item()
     assert item_name == 'Chicken Bowl'
-    assert order_id == 713926	
-    assert quantity == 159
+    assert quantity == 761
     total = solution.total_item_orders()
     assert total == 4972
     assert 39237.02 == solution.total_sales()
--- lab2/predict-car-price.py
+++ lab2/predict-car-price.py
@@ -4,19 +4,80 @@ import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+
+
+def get_rmse(r):
+    carprice=CarPrice()
+    w_0,w = carprice.train(r)
+    rmse_value = carprice.rmse(y_val, test(w_0, w,df_val))
+    return rmse_value
+
+def prepare_X(df):
+        base = ['engine_hp', 'engine_cylinders', 'highway_mpg', 'city_mpg', 'popularity']
+        df = df.copy()
+        features = base.copy()
+        df['age'] = 2020 - df.year
+        features.append('age')
+
+        for v in [2, 3, 4]:
+            feature = 'num_doors_%s' % v
+            df[feature] = (df['number_of_doors'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['chevrolet', 'ford', 'volkswagen', 'toyota', 'dodge']:
+            feature = 'is_make_%s' % v
+            df[feature] = (df['make'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['regular_unleaded', 'premium_unleaded_(required)', 
+                'premium_unleaded_(recommended)', 'flex-fuel_(unleaded/e85)']:
+            feature = 'is_type_%s' % v
+            df[feature] = (df['engine_fuel_type'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['automatic', 'manual', 'automated_manual']:
+            feature = 'is_transmission_%s' % v
+            df[feature] = (df['transmission_type'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['front_wheel_drive', 'rear_wheel_drive', 'all_wheel_drive', 'four_wheel_drive']:
+            feature = 'is_driven_wheens_%s' % v
+            df[feature] = (df['driven_wheels'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['crossover', 'flex_fuel', 'luxury', 'luxury,performance', 'hatchback']:
+            feature = 'is_mc_%s' % v
+            df[feature] = (df['market_category'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['compact', 'midsize', 'large']:
+            feature = 'is_size_%s' % v
+            df[feature] = (df['vehicle_size'] == v).astype(int)
+            features.append(feature)
+
+        for v in ['sedan', '4dr_suv', 'coupe', 'convertible', '4dr_hatchback']:
+            feature = 'is_style_%s' % v
+            df[feature] = (df['vehicle_style'] == v).astype(int)
+            features.append(feature)
+
+        df_num = df[features]
+        df_num = df_num.fillna(0)
+        X = df_num.values
+        return X
+
+
 class CarPrice:
 
     def __init__(self):
         self.df = pd.read_csv('data/data.csv')
-        print(f'${len(self.df)} lines loaded')
-        
-
+    
+     
     def trim(self):
         self.df.columns = self.df.columns.str.lower().str.replace(' ', '_')
         string_columns = list(self.df.dtypes[self.df.dtypes == 'object'].index)
         for col in string_columns:
             self.df[col] = self.df[col].str.lower().str.replace(' ', '_')
-
+   
     def validate(self):
         n = len(self.df)
         n_val = int(0.2 * n)
@@ -29,26 +90,66 @@ class CarPrice:
         df_train = df_shuffled.iloc[:n_train].copy()
         df_val = df_shuffled.iloc[n_train:n_train+n_val].copy()
         df_test = df_shuffled.iloc[n_train+n_val:].copy()
+
         y_train_orig = df_train.msrp.values
         y_val_orig = df_val.msrp.values
         y_test_orig = df_test.msrp.values
        
 
-        y_train = np.log1p(df_train.msrp.values)
-        y_val = np.log1p(df_val.msrp.values)
-        y_test = np.log1p(df_test.msrp.values)
+        y_train = np.log1p(y_train_orig)
+        y_val = np.log1p(y_val_orig)
+        y_test = np.log1p(y_test_orig)
 
         del df_train['msrp']
         del df_val['msrp']
         del df_test['msrp']
         
-        return ((df_train,df_val['msrp'],df_test['msrp']),( y_train,y_val,y_test))
+        return df_train,df_val,df_test,y_train,y_val,y_test
 
-    def linear_regression(self, X, y):
+    def linear_regression(self, X, y,r):
         ones = np.ones(X.shape[0])
         X = np.column_stack([ones, X])
 
         XTX = X.T.dot(X)
+        reg = r * np.eye(XTX.shape[0])
+        XTX = XTX + reg
         XTX_inv = np.linalg.inv(XTX)
         w = XTX_inv.dot(X.T).dot(y)
         return w[0], w[1:]
+    def rmse(self, y, y_pred):
+        error = y_pred - y
+        mse = (error ** 2).mean()
+        return np.sqrt(mse)
+   
+    def train(self, r):
+        X_train = prepare_X(df_train)
+        return self.linear_regression(X_train, y_train, r)
+
+    def display(self, X, y, y_pred):
+        columns = ['engine_cylinders','transmission_type','driven_wheels','number_of_doors',
+                   'market_category','vehicle_size','vehicle_style','highway_mpg','city_mpg','popularity']
+        X = X.copy()
+        X = X[columns]
+        X['msrp'] =np.expm1(y.round(2))
+        X['msrp_pred'] =np.expm1 (y_pred.round(2))
+        print(X.head(5).to_string(index=False))
+    
+
+def test(w_0, w, X):
+        carprice=CarPrice()
+        X = prepare_X(X)
+        return w_0 + X.dot(w)
+
+if __name__ == '__main__':
+    carprice=CarPrice()
+    carprice.trim()
+    df_train,df_val,df_test,y_train,y_val,y_test= carprice.validate()
+    r_min= min([0, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000], key=get_rmse)
+    print("best value of regulaarization parameter is:",r_min)
+    w0,w=carprice.train(r_min)
+    y_pred=test(w0,w,df_test)
+    print("Printing original msrp vs. predicted msrp of 5 cars") 
+    carprice.display(df_test,y_test,y_pred)
+    print("root mean square",carprice.rmse(y_test,y_pred))
+
+
