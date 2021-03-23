1e8a66906f6e9443e3f51f00b43ae164b829abc4
mercury297 <chinmay.kamerkar@somaiya.edu>
2021-03-22 19:56:43 -0700

    add lab3 changes

--- lab3/README.md
+++
@@ -1,8 +0,0 @@
-# Lab 3
-
-Try to build a number-5 detector binary classifier for the [MNIST](http://yann.lecun.com/exdb/mnist/) dataset from Scikit-Learn.
-
-Use lab3.py as baseline and [How to load MINIST Dataset](http://rasbt.github.io/mlxtend/user_guide/data/loadlocal_mnist/)
-
-
-
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,89 +1,89 @@
-import numpy as np
-import os
 import pandas as pd
-from sklearn.metrics import mean_squared_error
 
-# To plot pretty figures
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-mpl.rc('axes', labelsize=14)
-mpl.rc('xtick', labelsize=12)
-mpl.rc('ytick', labelsize=12)
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
-# Where to save the figures
-PROJECT_ROOT_DIR = "."
-IMAGE_DIR = "FIXME"
+        
+class DiabetesClassifier:
+    def __init__(self) -> None:
+        col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+        self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
+        # print(self.pima.head())
+        self.X_test = None
+        self.y_test = None
+        
 
-def save_fig(fig_id, tight_layout=True):
-  # path = os.path.join(PROJECT_ROOT_DIR, "images", IMAGE_DIR, fig_id + ".png")
-  # path = os.path.join(fig_id + ".png")
-  path = "five" + ".png"
-  print("Saving figure", fig_id)
-  if tight_layout:
-    plt.tight_layout()
-  plt.savefig(path, format='png', dpi=300)
-
+    def define_feature(self):
+        feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age',]
+        X = self.pima[feature_cols]
+        y = self.pima.label
+        return X, y
     
+    def train_test_split(self):
+        # split X and y into training and testing sets
+        X, y = self.define_feature()
+        X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
+        # train a logistic regression model on the training set
+        return X_train, y_train
 
-def random_digit(X):
-    # return '1'
-    # print('X keys maybe :',X)
-    some_digit = X.iloc[36000]
-    some_digit_image = some_digit.values.reshape(28, 28)
-    plt.imshow(some_digit_image, cmap = mpl.cm.get_cmap('binary'),interpolation="nearest")
-    plt.axis("off")
-
-    save_fig("some_digit_plot")
-    plt.show()
-    return some_digit
-
-   
-def load_and_sort():
-  try:
-    from sklearn.datasets import fetch_openml
-    mnist = fetch_openml(name='mnist_784', cache=True)
-    mnist.target = mnist.target.astype(np.int8)  # fetch_openml() returns targets as strings
-    sort_by_target(mnist)  # fetch_openml() returns an unsorted dataset
-  except ImportError:
-    from sklearn.datasets import fetch_mldata
-    mnist = fetch_mldata('MNIST original')
-  # mnist["data"], mnist["target"]
-  return mnist["data"], mnist["target"]
+    def predict(self,model):
+        y_pred_class = model.predict(self.X_test)
+        return y_pred_class
 
 
-
-def sort_by_target(mnist):
-    reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
-    reorder_test = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[60000:])]))[:, 1]
-    X_train = mnist.data.iloc[reorder_train]
-    y_train = mnist.target.iloc[reorder_train]
-    X_test = mnist.data.iloc[reorder_test + 60000]
-    y_test = mnist.target.iloc[reorder_test + 60000]
-    return X_train, y_train, X_test, y_test
+    def calculate_accuracy(self, result):
+        return metrics.accuracy_score(self.y_test, result)
 
 
-def train_predict(some_digit, X_train, y_train):
-    shuffle_index = np.random.permutation(60000)
-    X_train, y_train =  X_train.iloc[shuffle_index], y_train.iloc[shuffle_index]
-
-    # Example: Binary number 4 Classifier
-    y_train_5 = (y_train == 5)
-    # y_test_5 = (y_test == 5)
-
-    from sklearn.linear_model import SGDClassifier
-    SGD = SGDClassifier()
-    SGD.fit(X_train, y_train_5)
-    SGD.predict([some_digit])
-    return SGD
+    def examine(self):
+        dist = self.y_test.value_counts()
+        # print(dist)
+        percent_of_ones = self.y_test.mean()
+        percent_of_zeros = 1 - self.y_test.mean()
+        return self.y_test.mean()
     
-def calculate_cross_val_score(classifier, y_train_5, X_train):
-    from sklearn.model_selection import cross_val_score
-    print(cross_val_score(classifier, X_train, cv=3, scoring="accuracy"))
+    def confusion_matrix(self, result):
+        return metrics.confusion_matrix(self.y_test, result)
+    
+    def descision_tree(self,X_train,y_train):
+        from sklearn import tree
+        model = tree.DecisionTreeClassifier(random_state=0, max_depth=5)
+        model.fit(X_train,y_train)
+        return model
+
+    def logistic_reg(self,X_train,y_train):
+        model = LogisticRegression(max_iter=300)
+        model.fit(X_train, y_train)
+        return model
+
+    def support_vector(self,X_train,y_train):
+        from sklearn import svm
+        model = svm.SVC(kernel='rbf')
+        model.fit(X_train, y_train, )
+        return model
+
+if __name__ == "__main__":
+    classifer = DiabetesClassifier()
+    X_train, y_train =  classifer.train_test_split()
+    reg_model = classifer.logistic_reg(X_train,y_train)
+    result = classifer.predict(reg_model)
+    score = classifer.calculate_accuracy(result)
+    print(f"logistic Regression: score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"Logistic Regression: confusion_matrix=${con_matrix}")
+
+    support_vector_model = classifer.support_vector(X_train,y_train)
+    result = classifer.predict(support_vector_model)
+    score = classifer.calculate_accuracy(result)
+    print(f"Support Vector Machine: score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"Support Vector Machine: confusion_matrix=${con_matrix}")
 
 
-X,y = load_and_sort()
-some_digit = random_digit(X)
-X_train, X_test, y_train, y_test = X.iloc[:60000], X.iloc[60000:], y.iloc[:60000], y.iloc[60000:]
-# print(len(mnist))
-SGD = train_predict(some_digit, X_train, y_train)
-calculate_cross_val_score(SGD, y_train, X_train)
+    descision_tree_model= classifer.descision_tree(X_train,y_train)
+    result = classifer.predict(descision_tree_model)
+    score = classifer.calculate_accuracy(result)
+    print(f"Descision Tree: score={score}")
+    con_matrix = classifer.confusion_matrix(result)
+    print(f"Descision Tree: confusion_matrix=${con_matrix}")
